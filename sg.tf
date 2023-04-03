830ed03bd0216e975cafd285939d7f74f56bad49
radhika <radhika@emial.com>
2023-04-03 20:42:22 +0530

    added ingress block of port 22

--- sg.tf
+++ sg.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "lx_srvr_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "To ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
