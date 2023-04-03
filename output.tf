34a6d60041e266b8237f17c95d1f8a22d52a0f93
radhika <radhika@emial.com>
2023-04-03 20:41:11 +0530

    added private IP o/p block

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   description = "Public IP address of EC2 instance"
   value       = aws_instance.app_server.public_ip
 }
+
+output "private_ip" {
+  description = "Public IP address of EC2 instance"
+  value       = aws_instance.app_server.private_ip
+}
