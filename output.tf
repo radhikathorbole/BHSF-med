698b3713ed2d358d5dada42546ab6e3997a2bd95
radhika <radhika@emial.com>
2023-04-03 19:31:27 +0530

    added output block to print private IP address

--- output.tf
+++ output.tf
@@ -3,3 +3,9 @@ output "public_ip" {
   value       = aws_instance.app_server.public_ip
 
 }
+
+output "private_ip" {
+  description = "Public IP address of EC2 instance"
+  value       = aws_instance.app_server.private_ip
+
+}
